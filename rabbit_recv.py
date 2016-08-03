d508cd6f9fe854de287c0cde6d26d3f18b8e7c06
Alexa Herrera <alexa.herrera@digitaloperative.com>
2016-08-02 22:13:36 -0700

    More rabbits were born. They do that, you know.

--- rabbit_recv.py
+++ rabbit_recv.py
@@ -1,15 +1,18 @@
 import pika
+import time
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
-channel.queue_declare('hello')
+channel.queue_declare('task_queue')
 
 def callback(ch, method, properties, body):
 	print(" [x] Received %r" % body)
+	time.sleep(body.count(b'.'))
+	print(" [x] Done")
 
 
-channel.basic_consume(callback, queue='hello', no_ack=True)
+channel.basic_consume(callback, queue='task_queue', no_ack=True)
 print(" [*] Waiting for messages. To exit press ctrl+c.")
 
 channel.start_consuming()
