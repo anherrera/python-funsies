e22cf2b29875a33d646dd383a7a1a78d6cca8be7
the h is silent <lipho@users.noreply.github.com>
2016-08-21 13:34:35 -0700

    Create apple.py

---
+++ apple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env/
+if __name__=='__main__':
+
+    print '()()()()()()\r'
+    print '|\         |\r'
+    print '|.\. . . . |\r'
+    print '.  \       |\r'
+    print ' \.:\ . . .|\r'
+    print '  \o \     |\r'
+    print '   \. \. . |\r'
+    print '    \. \   |\r'
+    print '      \. \ |\r'
+    print '       \__\|\r'
